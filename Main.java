dc8f24c7bf049ba20259f7627ac4e065d665e191
ivanovroman4999-pixel <ivanovroman4999@gmail.com>
2025-09-04 21:56:59 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,12 +1,9 @@
-import java.io.PrintStream;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         System.out.println("Ivanov Roman"); //Выводит имя
         int income = 44000;
         int spending = 30000;
-        System.out.println("Итого (руб):"income-spending); //Выводит остаток
+        System.out.println("Остаток (руб):");
+        System.out.println(income-spending);//Выводит остаток
     }
 }
