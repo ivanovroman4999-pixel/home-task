15a2fa87fbc565f94187484759fc28e4d6892f2b
ivanovroman4999-pixel <ivanovroman4999@gmail.com>
2025-10-01 23:29:55 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,18 +1,14 @@
 import java.util.Scanner;
-import ru.netology.service.CustomService;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Введите цену товара (в руб.):");
-        int price = scanner.nextInt();
-        System.out.print("Введите вес товара (в кг.):");
-        int weight = scanner.nextInt();
-        int poshlina = calculateCustoms(price, weight);
-        System.out.println("Размер пошлины (в руб.) составит: " + poshlina);
+        System.out.print("Введите год в формате ****: ");
+        int year = scanner.nextInt();
+        if (year%4==0) {
+            System.out.println("В этом году 366 дней!");
+        } else {
+            System.out.println("В этом году 365 дней!");
+        }
     }
-    public static int calculateCustoms(int a, int b) {
-        int result = a/100 + b*100;
-        return result;
-     }
 }
