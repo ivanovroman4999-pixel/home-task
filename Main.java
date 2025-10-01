388b1ce72d2ec8ef5c2108655ac606b64bfffdf5
ivanovroman4999-pixel <ivanovroman4999@gmail.com>
2025-10-01 14:26:47 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,9 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Ivanov Roman"); //Выводит имя
-        int income = 44000;
-        int spending = 30000;
-        System.out.println("Остаток (руб):");
-        System.out.println(income-spending);//Выводит остаток
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите цену товара (в руб.):");
+        int price = scanner.nextInt();
+        System.out.print("Введите вес товара (в кг.):");
+        int weight = scanner.nextInt();
+        int poshlina = calculateCustoms(price, weight);
+        System.out.println("Размер пошлины (в руб.) составит: " + poshlina);
     }
+    public static int calculateCustoms(int a, int b) {
+        int result = a/100 + b*100;
+        return result;
+     }
 }
