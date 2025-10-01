e8a0a7543137097a744fd9efcd5455edcbe6e66d
ivanovroman4999-pixel <ivanovroman4999@gmail.com>
2025-10-02 00:03:09 +0300

    Update Dop zadanie.java

--- Dop zadanie.java
+++ Dop zadanie.java
@@ -3,12 +3,27 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Введите год в формате ****: ");
-        int year = scanner.nextInt();
-        if (year%4==0) {
-            System.out.println("В этом году 366 дней!");
-        } else {
-            System.out.println("В этом году 365 дней!");
-        }
+        int quantity = 0;
+       while (true) {
+           System.out.println("Введите год:");
+           int year = scanner.nextInt();
+           System.out.println("Введите количество дней:");
+           int days = scanner.nextInt();
+           if (year%4==0 && days == 366) {
+                quantity += 1;
+           } else if (year%4!=0 && days == 365) {
+               quantity += 1;
+           } else {
+               if (year % 4 == 0) {
+                   System.out.println("Неправильно! В этом году 366 дней!");
+                   System.out.println("Набрано очков: " + quantity);
+                   break;
+               } else {
+                   System.out.println("Неправильно! В этом году 365 дней!");
+                   System.out.println("Набрано очков: " + quantity);
+                   break;
+               }
+           }
+       }
     }
 }
