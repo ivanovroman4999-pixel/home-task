d42f3e82a9d044846485adb6a349a73be804e611
ivanovroman4999-pixel <ivanovroman4999@gmail.com>
2025-10-24 20:02:03 +0300

    Add files via upload

---
+++ Book.java
@@ -0,0 +1,41 @@
+public class Book {
+    public String title;
+    public int releaseYear;
+    public String author;
+    public int pages;
+
+
+    public Book(String title, int releaseYear, String author, int pages) {
+        this.title = title;
+        this.releaseYear = releaseYear;
+        this.author = author;
+        this.pages = pages;
+    }
+
+
+    public boolean isBig() {
+        if (pages > 500) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    public boolean matches(String word) {
+        if (title.contains(word) || author.contains(word)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public int estimatePrice(int pages) {
+        int price = pages * 3;
+        if (price < 250) {
+            return 250;
+        } else {
+            return price;
+        }
+    }
+}
--- Main.java
+++ Main.java
@@ -1,41 +1,19 @@
-import java.util.Scanner;
-
 public class Main {
-    public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
-        String[] products = {"Хлеб", "Молоко", "Крупа"};
-        int[] prices = {50, 14, 80};
-        int[] productQuantities = new int[products.length];
-        int sumProducts = 0;
-        for (int i = 0; i < products.length; i++) {
-            System.out.print(i+1+") " + products[i]);
-            System.out.println("-" + prices[i]+ " Руб/шт");
-        }
-        while (true) {
-            System.out.println("Выберите товар и количество или введите end");
-            String input = scanner.nextLine();
-            if ("end".equals(input)){
-                break;
-            }
-            String[] parts = input.split(" ");
-            int productNumber = Integer.parseInt(parts[0])-1;
-            int productCount = Integer.parseInt(parts[1]);
-            int currentPrice = prices[productNumber];
-            sumProducts += currentPrice*productCount;
-            productQuantities[productNumber] += productCount;
-        }
-        System.out.println("Ваша корзина:");
-        for (int i = 0; i < products.length; i++){
-            if (productQuantities[i] > 0) {
-                int currentProductPrice = prices[i];
-                int quantity = productQuantities[i];
-                int itemTotal = currentProductPrice * quantity;
-                System.out.println(products[i] + " " + quantity + " шт " +
-                        currentProductPrice + " руб/шт " +
-                        itemTotal + " руб в сумме");
-            }
-        }
-        System.out.println("Итоговая сумма корзины:" + sumProducts);
-    }
- }
+    public static void main(String[] args) {
+        Book book = new Book("Ядовитый клинок", 2021, "Anya", 506);
+        System.out.println(book.isBig());
+        System.out.println(book.matches("Abc"));
+        System.out.println(book.estimatePrice(book.pages));
 
+        Book book2 = new Book("Олимп", 2024, "Pikls", 25);
+        System.out.println(book2.isBig());
+        System.out.println(book2.matches("Pikls"));
+        System.out.println(book2.estimatePrice(book2.pages));
+
+        Book book3 = new Book("Я переродился торговым автоматом", 2022, "Petya", 307);
+        System.out.println(book3.isBig());
+        System.out.println(book3.matches("лся"));
+        System.out.println(book3.estimatePrice(book3.pages));
+
+    }
+}
